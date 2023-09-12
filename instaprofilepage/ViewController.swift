056caa4d6ba44aee6e7dc2fdeecb00c41c2a4f8c
user2rum <nauu400@naver.com>
2023-09-12 16:54:30 +0900

    RENAME: 첫 커밋

---
+++ instaprofilepage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  instaprofilepage
+//
+//  Created by t2023-m0075 on 2023/09/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+//first commit
